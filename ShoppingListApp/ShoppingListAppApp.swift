5914d806b45c0f61bf7316de6cb6f6be242f2274
Dmitriy <gvelko90@gmail.com>
2022-05-25 13:48:41 +0200

    Initial Commit

---
+++ ShoppingListApp/ShoppingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListAppApp.swift
+//  ShoppingListApp
+//
+//  Created by Dmitryi Velko on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
